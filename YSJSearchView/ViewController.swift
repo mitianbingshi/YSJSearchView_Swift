123a4005ef91c30d82e8ad4d0ea8d76ff6fa5c05
mitianbingshi <779372008@qq.com>
2016-04-20 14:18:31 +0800

    Initial Commit

---
+++ YSJSearchView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YSJSearchView
+//
+//  Created by 闫树军 on 16/4/20.
+//  Copyright © 2016年 闫树军. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
